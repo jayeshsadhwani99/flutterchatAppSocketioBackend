d9cd7627658f17f73bc71fd321c94ae88509f26d
unknown <jayeshsadhwani99@gmail.com>
2021-06-13 15:01:20 +0530

    Listen for the send message event and display the object

--- index.js
+++ index.js
@@ -27,20 +27,37 @@ app.use(express.json());
 // Allow CORS-Cross Origin Resouce Sharing ( Share data between different URL's)
 app.use(cors());
 
+// Get the number of users connected
+var clients = {};
+
 // Events on socket.io
 
 // When a connection event fires, or a connection
 // is established, we define a callback method
+// Here, the parameter (socket) is the object of the client
 io.on("connection", (socket) => {
     console.log("Connected")
 
     // Print the ID of the client
     console.log(socket.id, " JOINED");
 
-    // Listen to the event we created on the frontend ( test )
-    socket.on("/test", (msg) => {
-        // See what message is passed from the frontend
-        console.log(msg);
+    // Listen to the event we created on the frontend ( signin ) 
+    // To get the user ID 
+    socket.on("signin", (id) => {
+        // See what ID is passed from the frontend
+        console.log(id);
+
+        // Add the current socket to the clients object
+        // To show that to the other client
+        // Here, key of the value is ID of the user that is
+        // passed and the value is the socket
+        clients[id] = socket;
+
+    })
+
+    // Whenever a message is sent, this event is called
+    socket.on("message", (msg) => {
+        console.log(msg)
     })
 });
 
